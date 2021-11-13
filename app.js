6f96c7b0e8c7f5d33a69f4d305aad5f3c22950a4
Alejandro Curiel <alex_curielg@hotmail.com>
2021-11-13 10:13:18 -0600

    add styles new page the IA

--- app.js
+++ app.js
@@ -250,29 +250,26 @@ function start() {
 function stop() {
    clearInterval(timeStarted);
 }
-function punto(){
-   
-}
 
-function frame() {
-  confetti({
-    particleCount: 2,
-    angle: 60,
-    spread: 55,
-    origin: { x: 0 },
-    colors: colors,
-  });
-  confetti({
-    particleCount: 2,
-    angle: 120,
-    spread: 55,
-    origin: { x: 1 },
-    colors: colors,
-  });
+// function frame() {
+//   confetti({
+//     particleCount: 2,
+//     angle: 60,
+//     spread: 55,
+//     origin: { x: 0 },
+//     colors: colors,
+//   });
+//   confetti({
+//     particleCount: 2,
+//     angle: 120,
+//     spread: 55,
+//     origin: { x: 1 },
+//     colors: colors,
+//   });
 
-  if (Date.now() < Date.now() + 15000) {
-    requestAnimationFrame(frame);
-  }
-}
+//   if (Date.now() < Date.now() + 15000) {
+//     requestAnimationFrame(frame);
+//   }
+// }
 
-window.onload = frame();
+// window.onload = frame();
--- minimax.js
+++ minimax.js
@@ -1,18 +1,12 @@
-/**
- * El código se dividirá en 4 partes:
- * 1. Formato del juego normalmente.
- * 2. ¿Cómo determino a un ganador?
- * 3. Crear una IA básica y un anuncio del ganador.
- * 4. Utilizar el algoritmo Minimax.
- *
- * Fuentes bibliográficas:
- * https://es.wikipedia.org/wiki/Minimax
- * https://medium.freecodecamp.org/how-to-make-your-tic-tac-toe-game-unbeatable-by-using-the-minimax-algorithm-9d690bad4b37 
- */
-
+ const spanX = document.getElementById('x')
+ const spanO = document.getElementById('o')
+ const spanE = document.getElementById('e')
  let origBoard;
  let huPlayer ='O';
  let aiPlayer = 'X';
+ let contX = 0;
+ let contO = 0;
+ let contE = 0;
  const winCombos =[
    [0, 1, 2],
    [3, 4, 5],
@@ -28,6 +22,7 @@
  startGame();
  
  function selectSym(sym){
+   start();
    huPlayer = sym;
    aiPlayer = sym === 'O' ? 'X' :'O';
    origBoard = Array.from(Array(9).keys());
@@ -48,8 +43,55 @@
      cells[i].innerText = '';
      cells[i].style.removeProperty('background-color');
    }
+
  }
  
+  window.onload = () => {
+    m = 0;
+    s = 0;
+    mls = 0;
+    timeStarted = 0;
+    time = document.getElementById("time");
+    btnStart = document.getElementById("btn-start");
+    btnStop = document.getElementById("btn-stop");
+    btnReset = document.getElementById("btn-reset");
+    btnStart.addEventListener("click", start);
+    btnStop.addEventListener("click", stop);
+    btnReset.addEventListener("click", reset);
+  };
+
+  function write() {
+    let ht, mt, st, mlst;
+    mls++;
+
+    if (mls > 99) {
+      s++;
+      mls = 0;
+    }
+    if (s > 59) {
+      m++;
+      s = 0;
+    }
+    if (m > 59) {
+      h++;
+      m = 0;
+    }
+
+    mlst = ('0' + mls).slice(-2);
+    st = ('0' + s).slice(-2);
+    mt = ('0' + m).slice(-2);
+    
+    time.innerHTML = `${mt}:${st}.${mlst}`;
+  }
+
+  function start() {
+    write();
+    timeStarted = setInterval(write, 10);
+  }
+
+  function stop() {
+    clearInterval(timeStarted);
+  }
  function turnClick(square) {
    if (typeof origBoard[square.target.id] ==='number') {
      turn(square.target.id, huPlayer);
@@ -60,10 +102,16 @@
  
  function turn(squareId, player) {
    origBoard[squareId] = player;
+   console.log("jugador:"+player)
    document.getElementById(squareId).innerHTML = player;
+   if(document.getElementById(squareId).innerHTML === 'O'){
+    document.getElementById(squareId).style.color = "white";
+   }else {
+    document.getElementById(squareId).style.color = "black";
+
+   }
    let gameWon = checkWin(origBoard, player);
    if (gameWon) gameOver(gameWon);
-   checkTie();
  }
  
  function checkWin(board, player) {
@@ -86,6 +134,14 @@
    for (let i=0; i < cells.length; i++) {
      cells[i].removeEventListener('click', turnClick, false);
    }
+   console.log("EL ganador de la partida es: "+gameWon.player)
+   if (gameWon.player === 'X'){
+    ++contX;
+    spanX.textContent = contX
+   } else {
+    ++contO;
+    spanO.textContent = contO
+   }
    declareWinner(gameWon.player === huPlayer ? "¡Has ganado!" : "Perdiste.");
  }
  
@@ -104,16 +160,20 @@
  function checkTie() {
    if (emptySquares().length === 0){
      for (cell of cells) {
-       cell.style.backgroundColor = "yellow";
+       cell.style.backgroundColor = "gold";
        cell.removeEventListener('click',turnClick, false);
      }
      declareWinner("Empate.");
+     console.log("Me cicle")
+     ++contE;
+     spanE.textContent = contE
      return true;
    } 
    return false;
  }
  
  function minimax(newBoard, player) {
+   
    var availSpots = emptySquares(newBoard);
    
    if (checkWin(newBoard, huPlayer)) {
